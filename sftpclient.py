4b28da8b8308375eda40c2d7f65bf9f4c5a78679
apex <kingntop@naver.com>
2020-07-21 20:45:20 +0900

    0721

---
+++ sftpclient.py
@@ -0,0 +1,83 @@
+import paramiko
+import datetime, os
+from sftpconn import sftpconn
+
+paramikolog = '/home/mrp/Script/paramiko.log'
+now = datetime.datetime.now() - datetime.timedelta(minutes=10)
+YMD = now.strftime("%Y%m%d")
+HMD = now.strftime("%Y%M")
+
+YMD = '20200620'
+
+LOCAL  = '/home/mrp/Script/logs/'
+REMOTE = '/logs/uplus'
+DIRS  = ['/arentService11/tloLog/', '/arentService12/tloLog/']
+
+def set_Env():
+    with open("/home/limym/personalize/media/env.json") as json_file:
+        json_data = json.load(json_file)
+        global HOSTS, USERNAME, PASSWORD
+        HOSTS = json_data["server"]
+        USERNAME = json_data["id"]
+        PASSWORD = json_data["pwd"]
+        print (HOSTS)
+
+def check_dir(PATH):
+    try:
+        os.makedirs(PATH)
+    except OSError:
+        pass
+
+def remove_dir(PATH):
+    try:
+        os.makedirs(LOCAL + YMD)
+    except OSError:
+        pass
+
+def remove_basefile(PATH):
+    fileList = glob.glob(PATH + '*.log')
+    # shutil.rmtree(PATH)
+    print('remove_basefile_filelist', fileList)
+    for filePath in fileList:
+        try:
+            os.remove(filePath)
+        except:
+            print("Error while deleting file : ", filePath)
+
+def concatFiles(PATH, filename):
+    file_list = glob.glob(PATH+'*.log')
+    with open(PATH + filename, 'a') as merge_file :
+        print ('concate file lsit ', file_list)
+        for fname in file_list:
+            with open(fname, 'r') as f:
+                for line in f:
+                    merge_file.write(line)
+    
+
+def localfile_list (PATH):
+    arr = os.listdir(PATH)
+    print('file', arr)  
+    return arr
+
+
+if __name__ == "__main__":
+    set_Env()
+    local_base = LOCAL + YMD + '/';
+    remove_basefile(local_base);
+
+    print (HOSTS)
+
+    for host in HOSTS :
+        try :
+            f = sftpconn(paramikolog, USERNAME, PASSWORD, host, '22', False);
+            for servicenum in DIRS :
+                local_host = local_base + '/' + host + '/'
+                check_dir(local_host)
+                print(local_host)
+                lfiles = localfile_list(local_host)
+                f.get(lfiles, local_host, local_base, REMOTE + servicenum + YMD + '/')
+            f.close()
+        except Exception as e:
+            print(e)
+
+    concatFiles(local_base, YMD + HMD + '.txt')
---
+++ sftpconn.py
@@ -0,0 +1,126 @@
+import paramiko, os
+from os import path, access, R_OK
+
+
+class sftpconn(object):
+
+	rsa_private_key = r'/path/to/your/rsa.key'
+
+	def __init__(self, logfile, username, password, host, port, ssh_key):
+		paramiko.util.log_to_file(logfile)
+		
+		print ('Establishing SSH connection to:', host, port, '...')
+		self.transport = paramiko.Transport((host, int(port)))
+
+		if ssh_key == True:
+			sshkey = paramiko.RSAKey.from_private_key_file(self.rsa_private_key)
+			self.transport.connect(username = username, pkey = sshkey)
+
+		else:	
+			self.transport.connect(username = username, password = password)
+
+		self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+			
+	# check if the file exists
+	def check_file(self, PATH):
+		if path.exists(PATH) and path.isfile(PATH) and access(PATH, R_OK):
+			return 0
+		else:
+			return 1
+
+	# this function will allow the use of wildcards in between underscores, eg: file_*_name.txt
+	def is_match(self, a, b):
+		aa = a.split('_')
+		bb = b.split('_')
+		if len(aa) != len(bb): return False
+		for x, y in zip(aa, bb):
+			if not (x == y or x == '*' or y == '*'): return False
+		return True
+
+	
+	def get(self, file_formats, local_dir, remote_dir):
+		files_copied = 0
+		errors = 0
+		summary = ''
+		actual_files = []
+		remote_files = []
+		try:
+			for f in self.sftp.listdir(remote_dir):
+				remote_files.append(f)
+
+			difference = list(set(remote_files).difference(file_formats))
+			# print(difference)
+			# for file_format in file_formats:
+			for f in difference:
+				actual_files.append(remote_dir+f)
+
+			for actual_file in actual_files:
+				print ('actual_file:', actual_file)
+				base_file = actual_file.replace(remote_dir, '')
+				self.sftp.get(actual_file, local_dir + base_file)
+
+				files_copied += 1
+				summary += "[Copied] " + local_dir + base_file + '\n'
+
+			if errors > 0 or files_copied == 0:
+				print ('summary:', summary)
+				if files_copied == 0:
+					return [summary, "No files available for transfer.", 'No Files']
+				else:
+					return [summary, "This transaction failed with "+ str(errors) +" error/s:\n\n" + summary, 'Failed']
+			else:
+				return [summary, "Total file/s copied: %s. Summary: %s" % (str(files_copied), summary), 'Success']
+
+		except Exception as e:
+				print ('exception:', e)
+				return [summary, "Error while copying file : " + str(e), 'Failed']
+
+	def chdir(self, dir):
+    		self.sftp.chdir(dir)
+
+	def ls(self, remote):
+		return self.sftp.listdir(remote)
+
+	def close(self):
+		if self.transport.is_active():
+			self.sftp.close()
+			self.transport.close()
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, type, value, tb):
+		self.close()
+
+	# def mput(self, local, remote):
+	# 	files_copied = 0
+	# 	summary = ''
+	# 	try:
+	# 		for root, dirs, files in os.walk(local):
+	# 			print files
+	# 			for name in sorted(files):
+	# 				filename = os.path.join(root, name)
+	# 				self.sftp.put(filename, remote + name)
+	# 				files_copied += 1
+	# 				summary = summary + "Copied: " + remote + name + "\n"
+	# 		return [summary, "Total file/s copied: " + str(files_copied), 'Success']
+	# 	except Exception, e:
+	# 		return [summary, "Error: " + str(e), 'Failed']
+
+	def mget(self, lfile, local, remote):
+		files_copied = 0
+		summary = ''
+		try:
+
+			for f in self.sftp.listdir(remote):
+				print (f)
+				self.sftp.get(remote+f, local+f)
+				files_copied += 1
+				summary = summary + "Copied: " + remote + f + "\n"
+			print (summary)
+			return [summary, "Total file/s copied: " + str(files_copied), 'Success']
+		except Exception as e:
+			print (e)
+			return [summary, "Error: " + str(e), 'Failed']
+
+
